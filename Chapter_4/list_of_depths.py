c0bb44d0a0c7586d0efa99c82666478f5925b741
Scott Clements <clemencyoflee@gmail.com>
2022-01-28 12:12:45 -0800

    Chapter 4 Problem 3: Added more unit tests.

--- Chapter_4/list_of_depths.py
+++ Chapter_4/list_of_depths.py
@@ -30,20 +30,46 @@ def list_of_depths(binary_tree):
 
 class Test(unittest.TestCase):
 
-    tree = [8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15]
+    trees = [
+        [8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15],
+        [8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15, 16],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        [0, 1, 2, None, 4, None, 6, None, 8, None, 10, None, 12, None, 14], # Represents a full, but not complete binary tree
+    ]
 
-    test = [
+    tests = [
+        [
         deque([8]),
         deque([7, 9]),
         deque([6, 10, 5, 11]),
         deque([4, 12, 3, 13, 2, 14, 1, 15]),
+        ],
+        [
+        deque([8]),
+        deque([7, 9]),
+        deque([6, 10, 5, 11]),
+        deque([4, 12, 3, 13, 2, 14, 1, 15]),
+        deque([8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15, 16]),
+        ],
+        [
+        deque([0]),
+        deque([1, 2]),
+        deque([3, 4, 5, 6]),
+        deque([7, 8, 9, 10, 11, 12, 13, 14]),
+        ],
+        [
+        deque([0]),
+        deque([1, 2]),
+        deque([None, 4, None, 6]),
+        deque([None, 8, None, 10, None, 12, None, 14]),
+        ],
     ]
 
     def test_list_of_depths(self):
-        expected = self.test
-        actual = list_of_depths(self.tree)
-        print(f"Actual:  {actual}\nExpected:{expected}\n")
-        assert actual == expected
+        for expected, tree in zip(self.tests, self.trees):
+            actual = list_of_depths(tree)
+            print(f"Actual:  {actual}\nExpected:{expected}\n")
+            assert actual == expected
 
 if __name__ == "__main__":
     unittest.main()
