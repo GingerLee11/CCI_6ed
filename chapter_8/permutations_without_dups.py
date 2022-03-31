6c51edb7070a8935b6b64f7964500ac69a0e22a6
Scott Clements <clemencyoflee@gmail.com>
2022-03-31 13:47:40 -0700

    8.7 Implementation using the posted answers.

--- chapter_8/permutations_without_dups.py
+++ chapter_8/permutations_without_dups.py
@@ -1,40 +1,60 @@
 # python3
 # permutations_without_dups.py - Computes all the permutations for a given string with unique characters.
 
-from collections import deque
 
-
-def permutations_without_dups(text, char=None, perm=None, all_permutations=None):
+# Approach 1: building from permutations of first n-1 characters
+def get_perms(text):
     """
     Computes all the permutations for a given string with unique characters.
     """
-    if perm == None:
-        perm = ''
-
-    all_permutations = []
-    text_deque = deque(text)
-    while len(text_deque) != 0: 
-        char = text_deque.popleft()
-        perm += char
-        perm = perms_wo_dups(char, text_deque, perm)
-        all_permutations.append(perm)
-
-    return all_permutations
-
-
-def perms_wo_dups(char, text, perm=None):
-    """
-    Helper recursive function for permutations without dups
-    """
-    if perm == None:
-        perm = ''
+    permutations = []
+    if text is None:
+        return None
     
-    while len(text) != 0:
-        char_2 = text.popleft()
-        perm += char_2
-        perm = perms_wo_dups(char_2, text, perm)
+    # base case    
+    if len(text) == 0:
+        permutations.append(" ")
+        return permutations
+    
+    first = text[0] # get first letter in string
+    remainder = text[1:]
+    words = get_perms(remainder)
+    for word in words:
+        index = 0
+        for char in word:
+            s = insert_char_at(word, first, index)
+            permutations.append(s)
+            index += 1
+    return permutations
 
-    return perm
+
+def insert_char_at(word, char, i):
+    start = word[:i]
+    end = word[i:]
+    return start + char + end
 
 
-print(permutations_without_dups('abcd'))
+# Approach 2:
+def get_perms_2(text):
+    """
+    Building from permutations of all n-1 character substrings
+    """
+    result = []
+    get_perms_inner(" ", text, result)
+    return result
+
+def get_perms_inner(prefix, remainder, result):
+    if len(remainder) == 0:
+        result.append(prefix)
+    length = len(remainder)
+    for i in range(length):
+        before = remainder[:i]
+        after = remainder[1 + i :]
+        c = remainder[i]
+        get_perms_inner(prefix + c, before + after, result)
+
+
+if __name__ == "__main__":
+    print(get_perms('abcd'))
+    print()
+    print(get_perms_2('abcd'))
