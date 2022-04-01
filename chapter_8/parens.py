d3c5d2aae1bbf41868563cb16cb04a2b9258b035
Scott Clements <clemencyoflee@gmail.com>
2022-04-01 13:41:26 -0700

    8.9 Works for 1 and 2, but is not scalable.

--- chapter_8/parens.py
+++ chapter_8/parens.py
@@ -6,7 +6,7 @@ def parens(paren_pairs):
     Prints out all the parenthesis combonations possible given an inputted number of parenthesis pairs.
     """
     all_paren_combos = []
-    get_paren_combos(" ", paren_pairs, paren_pairs, all_paren_combos)
+    get_paren_combos("", paren_pairs, paren_pairs, all_paren_combos)
     return all_paren_combos
 
 
@@ -15,32 +15,29 @@ def get_paren_combos(paren, left, right, all_paren_combos):
     Helper function for parens
     """
     # Sanity check
-    if right < left:
-        raise Exception("Left parenthesis must be opened before right parenthesis.")
-    
-    # Base Case: 
-    if left == 0 and right == 0:
-        if paren not in all_paren_combos:
-            all_paren_combos.append(paren)
-            
-
+    # if right < left:
+        # raise Exception("Left parenthesis must be opened before right parenthesis.")
 
     left_count = left
     right_count = right
 
-    while left_count != 0:
-        paren += '(' 
-        # left_parens = paren[:left]
-        left_count -= 1
-        get_paren_combos(paren, left_count, right_count, all_paren_combos)
     
-    while right_count != 0:
-        paren += ')'
-        # right_parens = paren[right:]
-        right_count -= 1
-        get_paren_combos(paren, left_count, right_count, all_paren_combos)
+    for i in range(left_count, 0, -1):
+        paren += '(' * i
+        left_count -= i
+        paren += ')' * i
+        right_count -= i
+
+        # Base Case: 
+        if left_count == 0 and right_count == 0:
+            if paren not in all_paren_combos:
+                all_paren_combos.append(paren)
+        else:
+            get_paren_combos(paren[i] + paren[2 * i], left - 1, right - 1, all_paren_combos)
+        
 
 
 if __name__ == "__main__":
     print(parens(1))
     print(parens(2))
+    print(parens(3))
