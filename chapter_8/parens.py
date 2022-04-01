d6816a8e671d565139184045c3955005842013d0
Scott Clements <clemencyoflee@gmail.com>
2022-04-01 15:32:29 -0700

    8.9 Implementation using the answer key as a guide.

--- chapter_8/parens.py
+++ chapter_8/parens.py
@@ -1,43 +1,29 @@
 # python3
 # parens.py - Prints out all the parenthesis combonations possible given an inputted number of parenthesis pairs.
 
-def parens(paren_pairs):
+def parens(n):
     """
     Prints out all the parenthesis combonations possible given an inputted number of parenthesis pairs.
     """
     all_paren_combos = []
-    get_paren_combos("", paren_pairs, paren_pairs, all_paren_combos)
-    return all_paren_combos
 
-
-def get_paren_combos(paren, left, right, all_paren_combos):
-    """
-    Helper function for parens
-    """
-    # Sanity check
-    # if right < left:
-        # raise Exception("Left parenthesis must be opened before right parenthesis.")
-
-    left_count = left
-    right_count = right
-
-    
-    for i in range(left_count, 0, -1):
-        paren += '(' * i
-        left_count -= i
-        paren += ')' * i
-        right_count -= i
-
+    def get_paren_combos(paren, left_remaining, right_remaining):
+        """
+        Helper function for parens
+        """
         # Base Case: 
-        if left_count == 0 and right_count == 0:
+        if len(paren) == n * 2:
             if paren not in all_paren_combos:
-                if len(all_paren_combos) == 0:
-                    all_paren_combos.append(paren)
-                elif len(paren) == len(all_paren_combos[0]):
-                    all_paren_combos.append(paren)
-        else:
-            get_paren_combos(all_paren_combos[0][i : -i], left - 1, right - 1, all_paren_combos)
-        
+                all_paren_combos.append(paren)
+
+        if left_remaining > 0:
+            get_paren_combos(paren + '(', left_remaining - 1, right_remaining)
+            
+        if right_remaining > left_remaining:
+            get_paren_combos(paren + ')', left_remaining, right_remaining -1)
+
+    get_paren_combos("", n, n)
+    return all_paren_combos
 
 
 if __name__ == "__main__":
