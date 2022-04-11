2625ea2ce3d6f6f7e5108f7aebc7375ac86b60f0
Scott Clements <clemencyoflee@gmail.com>
2022-04-11 09:10:08 -0700

    8.14 Implementation following the answers on Github.

--- chapter_8/boolean_evaluation.py
+++ chapter_8/boolean_evaluation.py
@@ -1,50 +1,68 @@
 # python3
 # boolean_evaluation.py - Takes in a boolean expression and the desired result (True or False). 
 
-from distutils.util import strtobool
+def string_to_bool(s: str) -> bool:
+    return s == '1'
 
 
-def boolean_eval(boolean_exp, result=True, start='', remainder='', x=0, y=0, total_combos=0):
+def boolean_eval(exp: str, result: bool, memo) -> int:
     """
     Takes in a boolean expression and the desired result (True or False), defaults to True.
     Returns the number of parenthesis combinations possible to achieve the desired result.
     """
-
-    # If x is zero then this slice will only return one character so the whole string is removed.
-    if x == 0:
-        test_exp = start + boolean_exp[y:] + remainder
-    else:
-        test_exp = test_exp = start + boolean_exp[y:x] + remainder
-    test_exp = bool(strtobool(test_exp))
-
-    # Base Case
-    if test_exp == result: 
-        total_combos += 1
-
-    remainder = boolean_exp[x:]
-    x -= 2
-    remainder = boolean_exp[x:]
+    if len(exp) == 0:
+        return 0
+    if len(exp) == 1:
+        return 1 if string_to_bool(exp) == result else 0
     
-    if len(start) + len(remainder) >= len(boolean_exp) - 3:
-        start = boolean_exp[:y]
-        y += 2
-        start = boolean_exp[:y]
-        x = 0
-        remainder = ''
-    if '(' not in start:
-        start += '('
+    if exp + str(result) in memo:
+        return memo[exp + str(result)]
 
-    return boolean_eval(boolean_exp, result, start, ')' + remainder, x, y, total_combos)
+    ways = 0
+    for i in range(1, len(exp), 2):
+        left = exp[:i]
+        right = exp[i + 1 :]
+
+        left_true = boolean_eval(left, True, memo)
+        left_false = boolean_eval(left, False, memo)
+        right_true = boolean_eval(right, True, memo)
+        right_false = boolean_eval(right, False, memo)
+
+        total = (left_true + left_false) * (right_true + right_false)
+
+        total_true = 0
+        if exp[i] == "|":
+            total_true = (
+                left_true * right_true
+                + left_false * right_true
+                + left_true * right_false
+            )
+
+        elif exp[i] == '&':
+            total_true = left_true * right_true
+        elif exp[i] == '^':
+            total_true = left_true * right_false + left_false * right_true
+
+        subways = total_true if result else (total - total_true)
+        ways += subways
+
+    memo[exp + str(result)] = ways
+    return ways
+        
+
+def evaluate(exp: str, result: bool) -> int:
+    memo = {}
+    return boolean_eval(exp, result, memo)
 
 
 def example():
     # Sanity check:
-    print(boolean_eval("0", False))
-    print(boolean_eval("1", True))
+    print(evaluate("0", False))
+    print(evaluate("1", True))
 
     # Test the function
-    print(boolean_eval("1^0|0|1", False))
-    print(boolean_eval("0&0&0&1^1|0", True))
+    print(evaluate("1^0|0|1", False))
+    print(evaluate("0&0&0&1^1|0", True))
 
 if __name__ == "__main__":
     example()
